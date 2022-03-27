e4feedafeb94e21db110efed8e98cf312e3a5605
SamanjaCartagena <cart.samanja09@gmail.com>
2022-03-27 01:11:56 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const app=Vue.create({
+    data(){
+        return{
+            result:0,
+            num:0, 
+
+        }
+    },
+    methods:{
+        setResult(){
+          return  this.setResult= this.result;
+        },
+        num1(){
+
+        }
+    }
+
+})
+
+app.mount('#assignment')
